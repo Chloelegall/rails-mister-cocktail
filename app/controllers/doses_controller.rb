77a50bdcbc88949e6c651de5c8e90addb0143646
Chloe Le Gall <chloe.legall7@gmail.com>
2019-05-23 17:20:26 +0200

    cocktail tj

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -18,7 +18,6 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(params[:cocktail_id])
   end
 
   private
