516bd291a09536fcab9c78590f1d71c86b517f04
Chloe Le Gall <chloe.legall7@gmail.com>
2019-05-23 16:50:14 +0200

    Prepare rails app with external specs

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'doses', to "doses#index"
+  # get 'doses/:id', to: "doses#show", as: :dose
+  # get 'doses/new', to: "doses#new", as: :new_dose
+  # post 'doses', to: 'doses#create', as: :create_dose
+  # get 'doses/:id', to: 'doses#destroy', as: :destroy_dose
+  # get 'cocktails', to: "cocktails#index"
+  # get "cocktails/new", to: "cocktails#new", as: :new_cocktail
+  # get "cocktails/:id", to: "cocktails#show", as: :cocktail
+  # post "cocktails", to: "cocktails#create", as: :create_cocktail
+  # delete "cocktails/:id", to: "cocktails#destroy", as: :destroy_cocktail
+  resources :cocktails, only: [:new, :index, :update, :show, :create, :destroy] do
+    resources :doses, only: [:index, :new, :create] do
+      resources :ingredients, only: [:new, :create, :destroy]
+    end
+  end
 end
